bfe7a22c1954fcebfa2349cfae64301ab6def1b5
Abdelrahman Hazem Salah <133057869+bedohazem@users.noreply.github.com>
2026-07-13 05:23:40 +0300

    Update main.tsx

--- apps/web-admin/src/main.tsx
+++ apps/web-admin/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import App from './App'
-import './index.css'
+import './styles.css'
 
 import { AuthProvider } from './auth/AuthContext'
 
